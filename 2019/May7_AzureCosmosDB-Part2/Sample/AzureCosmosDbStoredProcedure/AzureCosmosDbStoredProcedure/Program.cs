568b32f23c93667ef8a937f79180c45a0e2d7b55
Dinesh Kumar Prabakaran <dinuswt22@gmail.com>
2019-05-07 16:52:51 +0530

    Azure Cosmos DB - Part 2 webinar
    Sample and PPT committed

---
+++ 2019/May7_AzureCosmosDB-Part2/Sample/AzureCosmosDbStoredProcedure/AzureCosmosDbStoredProcedure/Program.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using System;
+using System.Configuration;
+using System.IO;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace AzureCosmosDbStoredProcedure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(async () =>
+            {
+                var endpoint = ConfigurationManager.AppSettings["DocDbEndpoint"];
+                var masterKey = ConfigurationManager.AppSettings["DocDbMasterKey"];
+                var databaseName = "ToDoList";
+                var collectionName = "Items";
+                var storedProcedureName = "spHelloWorld";
+
+                using (var client = new DocumentClient(new Uri(endpoint), masterKey))
+                {
+                    Console.WriteLine("\r\n>>>>>>>>>>>>>>>> Creating Database <<<<<<<<<<<<<<<<<<<");
+                    // Create new database Object
+                    //Id defines name of the database
+                    var dbDefinition = new Database { Id = databaseName };
+                    var db = await client.CreateDatabaseIfNotExistsAsync(dbDefinition);
+                    Console.WriteLine($"Database {databaseName} created successfully");
+
+                    //Create new database collection
+                    Console.WriteLine("\r\n>>>>>>>>>>>>>>>> Creating Collection <<<<<<<<<<<<<<<<<<<");
+                    var collectionDefinition = new DocumentCollection { Id = collectionName };
+                    var collection = await client.CreateDocumentCollectionIfNotExistsAsync(UriFactory.CreateDatabaseUri(databaseName),
+                        collectionDefinition);
+                    Console.WriteLine($"Collection {collectionName} created successfully");
+
+                    var sprocBody = File.ReadAllText(@"..\..\StoredProcedures\spHelloWorld.js");
+                    var spDefinition = new StoredProcedure
+                    {
+                        Id = storedProcedureName,
+                        Body = sprocBody
+                    };
+
+
+                    ////Execute Store Procedure
+                    Item newItem = new Item
+                    {
+                        Category = "personal",
+                        Name = "Groceries",
+                        Description = "Pick up blueberries, red berries",
+                        Completed = true,
+                    };
+                    var result = await client.ExecuteStoredProcedureAsync<string>
+                        (UriFactory.CreateStoredProcedureUri(databaseName, collectionName, "createToDoItem"), new RequestOptions { PartitionKey = new PartitionKey(newItem.Category) }, newItem);
+                    Console.WriteLine($"Executed Store Procedure: response:{result.Response}");
+
+
+                    ////Create Store Procedure
+                    //StoredProcedure sproc = await client.CreateStoredProcedureAsync
+                    //    (UriFactory.CreateDocumentCollectionUri(databaseName, collectionName), spDefinition);
+                    //Console.WriteLine($"\r\nCreated Store procedure Id:{sproc.Id} ");
+
+                    ////Execute Store Procedure
+                    //var result = await client.ExecuteStoredProcedureAsync<string>
+                    //    (UriFactory.CreateStoredProcedureUri(databaseName, collectionName, storedProcedureName), new RequestOptions { PartitionKey = new PartitionKey("test") });
+                    //Console.WriteLine($"Executed Store Procedure: response:{result.Response}");
+
+                    ////Delete Store Procedure
+                    //await client.DeleteStoredProcedureAsync
+                    //    (UriFactory.CreateStoredProcedureUri(databaseName, collectionName, storedProcedureName));
+                    //Console.WriteLine("Stored Procedure Deleted Successfully");
+
+                    Console.ReadKey();
+                }
+            }).Wait();
+        }
+    }
+
+    public class Item
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "description")]
+        public string Description { get; set; }
+
+        [JsonProperty(PropertyName = "isComplete")]
+        public bool Completed { get; set; }
+
+        [JsonProperty(PropertyName = "category")]
+        public string Category { get; set; }
+    }
+}
---
+++ 2019/May7_AzureCosmosDB-Part2/Sample/AzureCosmosDbStoredProcedure/AzureCosmosDbStoredProcedure/StoredProcedures/spCompletedItems.js
@@ -0,0 +1,14 @@
+﻿function createToDoItem(itemToCreate) {
+
+    var context = getContext();
+    var container = context.getCollection();
+
+    var accepted = container.createDocument(container.getSelfLink(),
+        itemToCreate,
+        function (err, itemCreated) {
+            if (err) throw new Error('Error' + err.message);
+            context.getResponse().setBody(itemCreated.id);
+        });
+
+    if (!accepted) return;
+}
